1ad2cae849efc359af370ea317ee66ad731775d5
unknown <juan.rodriguezadr@uanl.edu.mx>
2024-09-24 12:33:18 -0600

    notas

---
+++ notas.R
@@ -0,0 +1,19 @@
+
+# ## Como meter un archivo  -----------------------------------------------
+
+library(redmis)
+"nombre" surce data(url)
+# en caso de que no aparesca el archivo revisar con este comando: view(nombre del archivo )
+
+#para importar datos no seguros se usa els siguiente comando <-read.csv
+
+#comando para la estadistica descriptiva 
+#Prueba t de Student (t-test)...... maneja dos variables 
+t.test(variable ~ grupo, data = datos)
+# tapply 
+#para saber la normlidad tomamos en cuenta el valor de p 
+
+
+
+
+#Anova... cuando tenemos mas de 3 observaciones que comparar 
